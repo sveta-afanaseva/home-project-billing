dcda747baf30feb40115fd3f78966d479149100d
Sveta Afanaseva <s.afanasieva@tinkoff.ru>
2023-05-18 21:45:08 +0300

    add create_app func

--- app/main.py
+++ app/main.py
@@ -4,16 +4,17 @@ from fastapi import FastAPI
 from app.api.routers import account
 from app.sql_app.database import init_models
 
-app = FastAPI(docs_url="/", redoc_url=None)
 
+def create_app() -> FastAPI:
+    app = FastAPI(docs_url="/", redoc_url=None)
 
-@app.on_event("startup")
-async def startup():
-    await init_models()
+    @app.on_event("startup")
+    async def startup():
+        await init_models()
 
-
-app.include_router(account.router)
+    app.include_router(account.router)
+    return app
 
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(create_app())
